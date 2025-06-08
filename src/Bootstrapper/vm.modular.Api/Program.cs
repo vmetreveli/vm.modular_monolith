1500215f0eff239bdb92e431e9b862c17aacbbab
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2025-06-08 20:02:51 +0400

    Update API default version to 1.0, adjust Swagger options constructor, and add development container configuration to solution items.

--- src/Bootstrapper/vm.modular.Api/Program.cs
+++ src/Bootstrapper/vm.modular.Api/Program.cs
@@ -49,7 +49,7 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddApiVersioning(options =>
     {
-        options.DefaultApiVersion = new ApiVersion(2, 0);
+        options.DefaultApiVersion = new ApiVersion(1, 0);
         options.AssumeDefaultVersionWhenUnspecified = true;
         options.ReportApiVersions = true;
     })
--- src/Bootstrapper/vm.modular.Api/Swagger/ConfigureSwaggerOptions.cs
+++ src/Bootstrapper/vm.modular.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -6,18 +6,11 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace vm.modular.Api.Swagger;
 
-public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) : IConfigureOptions<SwaggerGenOptions>
 {
-    private readonly IApiVersionDescriptionProvider _provider;
-
-    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
-    {
-        _provider = provider;
-    }
-
     public void Configure(SwaggerGenOptions options)
     {
-        foreach (var description in _provider.ApiVersionDescriptions)
+        foreach (var description in provider.ApiVersionDescriptions)
             options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
     }
 
