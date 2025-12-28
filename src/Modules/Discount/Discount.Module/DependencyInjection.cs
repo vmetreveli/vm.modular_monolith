71a91e4e6d4c2afe0674136bd0d05f923da7f920
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2025-12-28 12:29:38 +0400

    add Discount

---
+++ src/Modules/Discount/Discount.Module/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discount.Module;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddApplication(configuration);
+        services.AddInfrastructure(configuration);
+        return services;
+    }
+    
+}
